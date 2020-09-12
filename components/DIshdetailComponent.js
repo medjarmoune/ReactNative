eb3802074c0657f21eb6bbe9b27b3246aab53704
medjarmoune <mjarmoune1@gmail.com>
2020-09-12 23:01:55 +0100

    Social Sharing

--- components/DIshdetailComponent.js
+++ components/DIshdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, TextInput, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, TextInput, Button, StyleSheet, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -67,48 +67,65 @@ function RenderDish(props) {
             return true;
         }
     })
-    
-        if (dish != null) {
-            return(
-                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}
-                ref={this.handlViewRef}
-                {...panResponder.panHandlers}
-                >
-                    <Card style={{flex:1}}
-                    featuredTitle={dish.name}
-                    image={{uri: baseUrl + dish.image}}>
-                    
-                        <Text style={{margin: 10}}>
-                            {dish.description}
-                        </Text>
-                        <View style={styles.iconCenter}>
-                                <Icon
-                                    raised
-                                    reverse
-                                    name={props.favorite ? 'heart' : 'heart-o'}
-                                    type='font-awesome'
-                                    color='#f50'
-                                    onPress={() => props.favorite ? console.log('Aleardy favorite') : props.onPress()}
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
+    if (dish != null) {
+        return(
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}
+            ref={this.handlViewRef}
+            {...panResponder.panHandlers}
+            >
+                <Card style={{flex:1}}
+                featuredTitle={dish.name}
+                image={{uri: baseUrl + dish.image}}>
+                
+                    <Text style={{margin: 10}}>
+                        {dish.description}
+                    </Text>
+                    <View style={styles.iconCenter}>
+                            <Icon
+                                raised
+                                reverse
+                                name={props.favorite ? 'heart' : 'heart-o'}
+                                type='font-awesome'
+                                color='#f50'
+                                onPress={() => props.favorite ? console.log('Aleardy favorite') : props.onPress()}
 
-                                />
-                                <Icon
-                                    raised
-                                    reverse
-                                    name='pencil'
-                                    type='font-awesome'
-                                    color='#512DA8'
-                                    onPress={() =>props.handleComment()}
+                            />
+                            <Icon
+                                raised
+                                reverse
+                                name='pencil'
+                                type='font-awesome'
+                                color='#512DA8'
+                                onPress={() =>props.handleComment()}
 
-                                />
-                        </View>
+                            />
+                            <Icon
+                                raised
+                                reverse
+                                name='share'
+                                type='font-awesome'
+                                color='#51D2A8'
+                                style={styles.cardItem}
+                                onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} 
+                            />
+                    </View>
 
-                    </Card>
-                </Animatable.View>
-            );
-        }
-        else {
-            return(<View></View>);
-        }
+                </Card>
+            </Animatable.View>
+        );
+    }
+    else {
+        return(<View></View>);
+    }
 }
 
 function RenderComments (props){
